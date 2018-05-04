213019341aa82ed9cbb8d3531e95d52ed1ccc29e
ernandes NA <ernandes.dev@gmail.com>
2018-05-03 22:20:37 -0300

    finalizando ex9

--- ex/field.js
+++ ex/field.js
@@ -4,6 +4,7 @@ class Field extends Component {
     constructor(props){
         super(props)
         this.state = { value: props.initialValue }
+        this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event) {
@@ -14,9 +15,11 @@ class Field extends Component {
         return (
             <div>
                 <label>{this.state.value}</label>
-                <input onChange={this.handleChange} value={rhis.state.value} />
+                <input onChange={ this.handleChange} value={this.state.value } />
             </div>
         )
         
     }
 }
+
+export default Field
